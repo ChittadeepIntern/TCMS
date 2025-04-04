3f92d21441f5fca8c31882857ab6a19a01bf7022
Chittadeep Intern <chittadeep@ibarts.in>
2025-04-04 21:34:50 +0530

    feat: added the map polylines, markers and other things based on some data

--- lib/services/api/calculate_optimized_route_provider_service.dart
+++ lib/services/api/calculate_optimized_route_provider_service.dart
@@ -21,17 +21,36 @@ class CalculateOptimizedRouteProviderService {
 
     // Data to send in the request body
     Map<String, dynamic> data = {
-      'locations': {
-        "location": ["51.388997,-0.119022", "51.391915,-0.103666"]
+      "locations": {
+        "location": [
+          "51.388997,-0.119022",
+          "51.391915,-0.103666",
+          "51.395776,-0.100273",
+          "51.392388,-0.114059",
+          "51.380195,-0.116957",
+          "51.379285,-0.118341",
+          "51.398147,-0.102348",
+          "51.396234,-0.107899"
+        ]
       },
-      'vehicles': [
+      "vehicles": [
         {"id": "Vehicle 1", "start_index": 0},
+        {"id": "Vehicle 2", "start_index": 2},
+        {"id": "Vehicle 3", "start_index": 1},
+        {"id": "Vehicle 4", "start_index": 4},
+        {"id": "Vehicle 5", "start_index": 5}
       ],
       "jobs": [
-        {"id": "Job 1", "location_index": 1}
+        {"id": "Job 1", "location_index": 1},
+        {"id": "Job 2", "location_index": 3},
+        {"id": "Job 3", "location_index": 2},
+        {"id": "Job 4", "location_index": 0},
+        {"id": "Job 5", "location_index": 4},
+        {"id": "Job 6", "location_index": 5},
+        {"id": "Job 7", "location_index": 6},
+        {"id": "Job 8", "location_index": 7}
       ]
     };
-
 
     try {
       return await _dio.post(url, queryParameters: queryParameters, data: data);
--- lib/view/pages/route_view.dart
+++ lib/view/pages/route_view.dart
@@ -12,65 +12,89 @@ class RouteView extends StatelessWidget {
     routeController.setupOptimizedRouteModel();
 
     return ScaffoldPage(
-        content: Column(children: [
-      Image.network(
-          'https://api.nextbillion.io/maps/optimal/static/33.93,-118.12,9/512x512.png?key=ffdfe373f0b74a7e8414accc57dfa927'),
-      Consumer<RouteController>(
-        builder:
-            (BuildContext context, RouteController controller, Widget? child) {
-          GetOptimizedRouteModel? optimizedRouteModel =
-              controller.optimizedRouteModel;
-          Result? result = controller.optimizedRouteModel?.result;
-          Summary? summary = result?.summary;
-          //List<Routes>? routes = result?.routes;
-          return optimizedRouteModel == null
-              ? Text("Data not loaded")
-              : Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: [
-                        Text("cost"),
-                        Text(summary!.cost.toString()),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text("routes"),
-                        Text(summary.routes.toString()),
-                      ],
-                    ),
+      content: SingleChildScrollView(
+        // Wrap in a scrollable widget
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // Adjust alignment
+          children: [
+            // Center the image
+            Center(
+              // child: Image.network(
+              //   'https://api.nextbillion.io/maps/optimal/static/51.388997,-0.119022,51.391915,-0.103666/512x512.png?key=ffdfe373f0b74a7e8414accc57dfa927&path=stroke:green|width:3|fill:none|enc:e|sxHjhVjEEAuAAmB?qAx@{B|@kCvBkGg@gCIYm@}BmAyEy@}CWcACYHC^IHCFAIm@s@uEm@_EM{@a@wCWkBKy@SEk@KQEOKOWaAyA]g@EHGACODKH@HUSc@Sc@}@sBcCgAkA{@e@_@KGqAaA??&markers=51.388997,-0.119022,red|51.391915,-0.103666,blue',
+              // ),
+              child: Image.network('https://api.nextbillion.io/maps/nonhybrid/static/auto/1200x300.png?key=ffdfe373f0b74a7e8414accc57dfa927&path=stroke:green|width:3|fill:none|enc:ofuxH|qR??_D`EuAbAsCdCGF}@p@GDeFzEEHEPJFHLdAlCPd@w@hAa@jAv@dArCfIx@hC|@vCdDuCl@g@r@i@FEr@g@XpALf@n@pBDLx@hC~@rC`@rAjCfI^hATt@x@~B@FFNZ|@LGVI|@WNGZKDNPp@??&markers=51.388997,-0.119022,red|51.391915,-0.103666,blue&path=stroke:blue|width:3|fill:none|enc:e|sxHjhVjEEAuAAmB?qAx@{B|@kCvBkGg@gCIYm@}BmAyEy@}CWcACYHC^IHCFAIm@s@uEm@_EM{@a@wCWkBKy@SEk@KQEOKOWaAyA]g@EHGACODKH@HUSc@Sc@}@sBcCgAkA{@e@_@KGqAaA??'),
+            ),
+            Consumer<RouteController>(
+              builder: (BuildContext context, RouteController controller,
+                  Widget? child) {
+                GetOptimizedRouteModel? optimizedRouteModel =
+                    controller.optimizedRouteModel;
+                Result? result = controller.optimizedRouteModel?.result;
+                Summary? summary = result?.summary;
+                List<Routes>? routes = result?.routes;
 
-                    Column(
-                      children: [
-                        Text("duration"),
-                        Text(summary.duration.toString()),
-                      ],
-                    ),
+                if (optimizedRouteModel == null) {
+                  return Text("Data not loaded");
+                }
 
-                    Column(
-                      children: [
-                        Text("distance"),
-                        Text(summary.distance.toString())
-                      ],
-                    ),
-
-                    // Row(
-                    //   children: [
-                    //     Flexible(
-                    //       child: ListView.builder(
-                    //         itemCount: routes!.length,
-                    //         itemBuilder: (context, index) => ListTile(
-                    //           leading: Text(routes[index].vehicle!),
-                    //         ),
-                    //       ),
-                    //     )
-                    //   ],
-                    // )
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment
+                        .start, // Adjust alignment for columns
+                    children: [
+                      // Display the summary details in a single row
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _buildInfoColumn(
+                              'Cost', summary?.cost.toString() ?? 'N/A'),
+                          _buildInfoColumn(
+                              'Routes', summary?.routes.toString() ?? 'N/A'),
+                          _buildInfoColumn('Duration',
+                              summary?.duration.toString() ?? 'N/A'),
+                          _buildInfoColumn('Distance',
+                              summary?.distance.toString() ?? 'N/A'),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      // Display the routes in a list
+                      Text("Routes",
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      SizedBox(height: 10),
+                      SizedBox(
+                        height: 200, // Set a fixed height for the ListView
+                        child: ListView.builder(
+                          itemCount: routes?.length ?? 0,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              leading: Text(routes![index].vehicle ?? 'N/A'),
+                              title: Text('Route ${index + 1}'),
+                              trailing: FilledButton(
+                                  onPressed: () {}, child: Text('Push Route')),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 );
-        },
-      )
-    ]));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Helper method to build info columns
+  Widget _buildInfoColumn(String title, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        Text(value),
+      ],
+    );
   }
 }
