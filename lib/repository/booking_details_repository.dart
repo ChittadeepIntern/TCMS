be78038dfdaaa21764c75ffdfb3173fca4208b40
Chittadeep Intern <chittadeep@ibarts.in>
2025-04-11 21:57:05 +0530

    -feat: handled issue for failure of booking details

--- lib/repository/booking_details_repository.dart
+++ lib/repository/booking_details_repository.dart
@@ -7,12 +7,16 @@ class BookingDetailsRepository {
 
   Future<BookingDetailsModel> getBookingDetails(
       String bookingId, String username, String authKey) async {
-    Response response = await _bookingDetailsService.getBookingDetails(
-        bookingId, username, authKey);
-    if (response.statusCode == 200) {
-      return BookingDetailsModel.fromJson(response.data);
-    } else {
-      throw Exception('Failed to load booking details');
+    try {
+      Response response = await _bookingDetailsService.getBookingDetails(
+          bookingId, username, authKey);
+      if (response.statusCode == 200) {
+        return BookingDetailsModel.fromJson(response.data);
+      } else {
+        throw Exception('Failed to load booking details');
+      }
+    } catch (e) {
+      throw Exception('Failed to load booking details: $e');
     }
   }
 }
